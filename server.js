0587b9ee4adac32dcb3dd19b45822915738763b9
Loganathan G P <logusivam26@gmail.com>
2024-12-10 12:46:24 +0000

    Dec 10, 2024, 6:16 PM

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(cors());
 
 // Serve static files (e.g., index.html, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
+// Catch-all route to serve frontend (e.g., index.html)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
 // Use the connection string and port from environment variables
